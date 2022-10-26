9a70d185f2cf3ee9bbeef6439eb1530c832555a6
Hugo Istillarte <99299132+superchugo@users.noreply.github.com>
2022-10-25 23:56:31 -0300

    Add files via upload

--- Clase 6/Program.cs
+++ Clase 6/Program.cs
@@ -1,1 +1,22 @@
+﻿Console.Clear();
+int respuesta;
+int intentos = 0;
+int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+do
+{
+    Console.WriteLine("Por favor ingrese el número secreto");
+    respuesta = int.Parse(Console.ReadLine());
+    intentos++;
+    if (respuesta > numeroSecreto)
+        Console.WriteLine("El número ingresado es mayor al número secreto");
+    else if (respuesta < numeroSecreto)
+            Console.WriteLine("El número ingresado es menor al número secreto");
+        else    
+        {
+            Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}");
+            Console.WriteLine($"Lo has logrado en {intentos} intentos!!");
+        }
+} while (numeroSecreto != respuesta);
 
+Console.WriteLine("Presione una tecla para finalizar la ejecución");
+Console.ReadKey();    
