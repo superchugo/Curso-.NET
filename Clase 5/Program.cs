bbdfdd81a98f32e67b819f1d701c25d5c51f35bd
Hugo Istillarte <99299132+superchugo@users.noreply.github.com>
2022-10-25 23:53:51 -0300

    Add files via upload

--- Clase 5/Program.cs
+++ Clase 5/Program.cs
@@ -1,1 +1,18 @@
+﻿string respuesta = "S";
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Por favor ingrese su nombre");
+    String nombre = Console.ReadLine();
+    Console.WriteLine($"Hola {nombre}");
+    Console.WriteLine("Desea continuar? S/N");
+    respuesta = Console.ReadLine();
+}
 
+if (respuesta.ToUpper() == "N")
+    Console.WriteLine("Programa finalizado correctamente");
+else
+    Console.WriteLine("Opcion no valida");
+
+Console.WriteLine("Presione una tecla para finalizar la ejecucion");
+Console.ReadKey();    
